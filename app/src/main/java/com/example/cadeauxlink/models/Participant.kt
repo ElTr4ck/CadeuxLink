c0cef75e21c0d0f8bbee7f416ab910daeb99494e
ElTr4ck <jozafat112015@gmail.com>
2025-01-03 01:38:27 -0600

    Modificaciones para nuevas vistas

--- app/src/main/java/com/example/cadeauxlink/models/Participant.kt
+++ app/src/main/java/com/example/cadeauxlink/models/Participant.kt
@@ -3,5 +3,6 @@ package com.example.cadeauxlink.models
 data class Participant(
     val name: String = "",
     val email: String = "",
-    val status: String = ""
+    val status: String = "",
+    val phone: String = ""
 )
